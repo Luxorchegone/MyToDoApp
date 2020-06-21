5e39008caecbb74226c41b88729071a19c37de7d
Luxorchegone <59573920+Luxorchegone@users.noreply.github.com>
2020-06-21 15:32:36 +0300

    Third commit
    Прикручены стейты, для выделения важных задач и заеркивания выполненных.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import "./todo-list-item.css"
 
-const TodoListItem = ({label, important = false}) => {
-    
-    const style = {
-        color: important ? 'steelblue' : 'Black',
-        fontWeight: important ? 'bold' : 'normal'
+export default class TodoListItem extends Component {
+ 
+    state = {
+        done: false,
+        important : false
     };
-    
-    
-    return (
-        <span className="todo-list-item">
-            <span 
-                className="todo-list-item-label"
-                style = { style } > 
-                { label } 
-            </span> 
-        
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-            <i className="fa fa-exclamation" />
-        </button>
 
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-            <i className="fa fa-trash-o" />
-        </button>
-        </span>
+    onLabelClick = () => {
+        this.setState(({done}) => {
+        return {
+            done: !done
+        }
+    });
+    };
+
+    onMarkImpotant = () => {
+        this.setState(({important}) => {
+        return {
+            important: !important
+        }
+    });
+    };
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        };
+
+        if (important) {
+            classNames += ' important';
+        };
+        
+
+
+        return (
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    onClick={this.onLabelClick}>
+                    {label}
+                </span>
+
+                <button type="button"
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onMarkImpotant}>
+                    <i className="fa fa-exclamation" />
+                </button>
+
+                <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                    <i className="fa fa-trash-o" />
+                </button>
+            </span>
         );
+    };
 };
 
-export default TodoListItem;
+
